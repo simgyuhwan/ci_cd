087e9372a5e5482c1ffe83cbb43412fd76ddcd45
sgh1939 <rbghks19@naver.com>
2022-12-26 11:36:20 +0900

    config : module init

---
+++ module_1/module-stream/src/main/java/com/example/modulestream/ModuleStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.modulestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleStreamApplication.class, args);
+    }
+
+}
